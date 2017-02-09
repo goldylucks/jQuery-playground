0be549de49b98d01d1813112dc9e61fc9c421b39
Adam Goldman <adamgoldman7@gmail.com>
2017-02-10 07:34:44 +0800

    gabriel

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ $(document).ready(onReady)
 
 function onReady () {
   console.log('init!')
-  
+  const $headerClone = $('#table-header').clone()
+                                        .attr('id', 'table-header-clone')
+                                        .addClass('fixed-header')
+                                        .prependTo('#search-results')
 
-
-
-  
+  $('#search-results').on('scroll', function () {
+    $headerClone.css('top', $(this).scrollTop())
+  })
   console.log('done!')
 }
