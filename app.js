e36110a4ddf8e357e58c7883d1a340572277241e
Adam Goldman <adamgoldman7@gmail.com>
2017-02-15 02:20:52 +0800

    firefox stickey row

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ $(document).ready(onReady)
 
 function onReady () {
   console.log('init!')
-  const $headerClone = $('#table-header').clone()
-                                        .attr('id', 'table-header-clone')
-                                        .addClass('fixed-header')
-                                        .prependTo('#search-results')
 
-  $('#search-results').on('scroll', function () {
-    $headerClone.css('top', $(this).scrollTop())
-  })
-  console.log('done!')
+const tableWidth = $('.search-results').outerWidth()
+const tableHeaderHeight = $('#table-header').height()
+const $headerClone = $('<div />').addClass('fixed-header').width(tableWidth).height(tableHeaderHeight)
+
+$('#search-results th').each(function () {
+    $('<div>' + $(this).html() + '</div>')
+    .appendTo($headerClone)
+})
+
+$('.search-results thead tr').hide()
+
+$headerClone.prependTo('#search-results')
+
+// const $columnClone = $('<div />')
+// const columnWidth = $('.country').eq(0).outerWidth()
+
+// $('tbody.list .country').each(function () {
+//   const $this = $(this)
+//   $('<div>' + $(this).html() + '</div>')
+//     .height($this.outerHeight())
+//     .appendTo($columnClone)
+// })
+// $columnClone
+//   .addClass('fixed-column')
+//   .outerWidth(columnWidth)
+//   .css('top', $headerClone.outerHeight())
+//   .prependTo('#search-results')
+
+  
 }
