66a5c5d103cc77b4330d7ff0e935aae95c9be167
Adam Goldman <adamgoldman7@gmail.com>
2017-02-15 03:37:17 +0800

    add browser detection

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 $(document).ready(onReady)
 
-  function onReady () {
-    console.log('init!')
+// Firefox 1.0+
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+// Internet Explorer 6-11
+const isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+const isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1+
+const isChrome = !!window.chrome && !!window.chrome.webstore;
+
+console.log('isFirefox', isFirefox)
+console.log('isSafari', isSafari)
+console.log('isIE', isIE)
+console.log('isEdge', isEdge)
+console.log('isChrome', isChrome)
+
+function onReady () {
+  console.log('init!')
 
   const $tableContainer = $('#search-results')
 
